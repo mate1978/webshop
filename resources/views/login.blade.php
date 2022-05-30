977c43c7a38c28d73551f065f048539e226477a1
mate1978 <mate.szabolcs2@gmail.com>
2022-05-30 22:17:33 +0200

    Commit message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,14 +6,15 @@
     <div class="container custom-login">
     <div class="row">
     <div class="col-sm-4 col-sm-offset-6">
-        <form>
+        <form action="login" method="POST">
             <div class="form-group">
+                @csrf
                 <label for="exampleInputEmail1">Email cím</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email cím megadása">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email cím megadása">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Jelszó</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Jelszó megadása">
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Jelszó megadása">
             </div>
 
         <button type="submit" class="btn btn-primary">Beküldés</button>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
